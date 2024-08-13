20c5d1e219c0245217cec9c825e5fa7e8bcd66b1
AngryHickory <jimsayles@hotmail.com>
2024-08-13 16:14:07 -0700

    Created function to get human choice

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-console.log("Hello World");
+console.log("Rock, Paper, Scissors Game!");
 
+const humanScore = 0;
+const computerScore = 0;
+
+function getHumanChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+    if (humanChoice === "rock") {
+        return options[0];
+    } else if (humanChoice === "paper") {
+        return options[1];
+    } else if (humanChoice === "scissors") {
+        return options[2];
+    }
+    return humanChoice;
+  }
+  const humanChoice = getHumanChoice();
+  console.log(humanChoice);
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
@@ -13,4 +30,4 @@ function getComputerChoice() {
   }
 
   const computerChoice = getComputerChoice();
-  console.log(computerChoice);
+  console.log("Computer chose:", computerChoice);
