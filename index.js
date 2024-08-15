92a2ba56ac48b3c268da56d0d6343129b1c63f1c
AngryHickory <jimsayles@hotmail.com>
2024-08-15 13:11:11 -0700

    Added functions to play 5 rounds in a game and keep score.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 console.log("Rock, Paper, Scissors Game!");
+// Score:
+let humanScore = 0;
+let computerScore = 0;
 
-const humanScore = 0;
-const computerScore = 0;
-
+// Human Choice:
 function getHumanChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     const humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
@@ -12,12 +13,14 @@ function getHumanChoice() {
         return options[1];
     } else if (humanChoice === "scissors") {
         return options[2];
+    } else {
+        console.log("Invalid choice. Please enter Rock, Paper, or Scissors.");
+        return getHumanChoice()
     }
-    return humanChoice;
   }
-  const humanChoice = getHumanChoice();
-  console.log(humanChoice);
 
+
+//Computer Choice:
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
@@ -29,5 +32,47 @@ function getComputerChoice() {
     }
   }
 
-  const computerChoice = getComputerChoice();
-  console.log("Computer chose:", computerChoice);
+//Play Round:
+  function playRound(humanChoice, computerChoice) {
+    const options = ["Rock", "Paper", "Scissors"];
+    if (humanChoice === computerChoice) {
+      return "Tie";
+    } else if ((
+        (humanChoice === options[0] && computerChoice === options[2]) ||
+        (humanChoice === options[1] && computerChoice === options[0]) ||
+        (humanChoice === options[2] && computerChoice === options[1])
+      )) {
+      humanScore++;
+      return "Player Wins!";
+    } else {
+      computerScore++;
+      return "Computer Wins!";
+    }
+  }
+
+  
+
+  //Play Game:
+  function playGame() {
+    for (let i = 0; i < 5; i++) {
+      const humanChoice = getHumanChoice();
+      console.log("You chose:", humanChoice);
+        
+      const computerChoice = getComputerChoice();
+      console.log("Computer chose:", computerChoice);
+        
+      const roundResult = playRound(humanChoice, computerChoice);
+      console.log(roundResult); 
+      
+      console.log("The score is: ", humanScore, "to", computerScore);
+
+      
+    }
+    console.log("The Final Score is:");
+    console.log("You:", humanScore, "Computer:", computerScore);
+    console.log("Refresh to play again!");
+  }
+
+  
+
+  playGame();
